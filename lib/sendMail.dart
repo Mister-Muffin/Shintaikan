d7049d19a034521192ddde79dfd4e9a3aca3b293
Mister-Muffin <markus.aa84@gmail.com>
2020-08-29 23:32:33 +0200

    Bug fixes & Cleanup

--- lib/sendMail.dart
+++ lib/sendMail.dart
@@ -2,6 +2,7 @@ import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
 
+//TODO: PLS HELP! 😥
 class SendMail extends StatefulWidget {
   static const routeName = '/mail';
   @override
@@ -18,76 +19,99 @@ class _SendMailState extends State<SendMail> {
   bool loading = false;
   String cloudFuncReturnStatus = "";
 
-  final HttpsCallable callable = CloudFunctions(region: 'europe-west1').getHttpsCallable(
-    functionName: 'sendEmail',
+  final HttpsCallable callable =
+      CloudFunctions(region: 'europe-west1').getHttpsCallable(
+    functionName: 'sendEmails',
   );
 
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Text("Kontakt & Feedback"),
-        actions: [IconButton(icon: Icon(OMIcons.send), onPressed: sendMessage)],
+        actions: [
+          IconButton(
+              icon: Icon(OMIcons.send),
+              onPressed: () {
+                sendMessage(formKey);
+              })
+        ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(mainAxisSize: MainAxisSize.max, children: [
-          Text("Hier kannst du die Geschäftsstelle, Silvia kontaktieren:",
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
-              )),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Betreff'),
-            onChanged: (String value) {
-              subject = value;
-            },
+        padding: const EdgeInsets.all(12),
+        child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Column(mainAxisSize: MainAxisSize.max, children: [
+              Text("Hier kannst du uns kontaktieren:",
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  )),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Deine Email-Adresse *'),
+                /* onChanged: (String value) {
+                  setState(() {
+                    email = value;
+                  });
+                },*/
+                validator: (String value) {
+                  if (value.toString().isEmpty) return "ALLA!";
+
+                  bool emailValid = RegExp(
+                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                      .hasMatch(value);
+                  return emailValid ? null : 'NE! So nich';
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Betreff'),
+                onChanged: (String value) {
+                  subject = value;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nachicht *'),
+                minLines: 1,
+                maxLines: 20,
+                maxLength: 500,
+                maxLengthEnforced: false,
+                onChanged: (String value) {
+                  message = value;
+                },
+                validator: (String value) {
+                  return value.toString().isNotEmpty ? null : "is leer :(";
+                },
+              ),
+              Container(
+                height: 20,
+              ),
+              messageStatusWidget(),
+            ]),
           ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Deine Email'),
-            onChanged: (String value) {
-              setState(() {
-                email = value;
-              });
-            },
-            validator: (String value) {
-              return value.contains('@') ? null : 'Ungültige E-Mail Adresse';
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Nachicht - Pflichtfeld'),
-            onChanged: (String value) {
-              message = value;
-            },
-          ),
-          Container(
-            height: 20,
-          ),
-          noEmailWarning(),
-          messageStatusWidget(),
-        ]),
+        ),
       ),
     );
   }
 
-  void sendMessage() async {
+  void sendMessage(GlobalKey<FormState> formKey) async {
+    if (!formKey.currentState.validate()) return;
     setState(() {
       showStatusWidget = true;
       loading = true;
     });
     try {
       if (email.isEmpty) email = "[Keine Email angegeben]";
-      final HttpsCallableResult result = await callable.call(
+      await callable.call(
         <String, dynamic>{
           'email': email,
           'subject': subject,
           'message': message
         },
       );
-      print(result.data);
       setState(() {
-        cloudFuncReturnStatus = result.data.toString();
         success = true;
         showStatusWidget = true;
       });
@@ -113,28 +137,6 @@ class _SendMailState extends State<SendMail> {
     });
   }
 
-  Widget noEmailWarning() {
-    if (email.isEmpty) {
-      return Row(
-        children: [
-          Icon(
-            OMIcons.warning,
-            color: Colors.orange,
-          ),
-          Text('Ohne Email können wir dir nicht Antworten.',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
-              ),
-              textAlign: TextAlign.center)
-        ],
-      );
-    } else {
-      return Container();
-    }
-  }
-
   Widget messageStatusWidget() {
     if (showStatusWidget && !loading) {
       if (success) {
@@ -153,14 +155,15 @@ class _SendMailState extends State<SendMail> {
                 textAlign: TextAlign.center)
           ],
         );
-      } else if (!success) {
+      } else {
         return Row(
           children: [
             Icon(
               OMIcons.error,
               color: Colors.red,
             ),
-            Text('Ein Fehler ist aufgetreten!\nSollte das Problem wiederholt auftreten,\nschreib bitte eine Mail an shintaikan@web.de.',
+            Text(
+                'Ein Fehler ist aufgetreten!\nSollte das Problem wiederholt auftreten,\nschreib bitte eine Mail an shintaikan@web.de.',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 14,
@@ -169,7 +172,7 @@ class _SendMailState extends State<SendMail> {
                 textAlign: TextAlign.justify)
           ],
         );
-      } else {}
+      }
     } else if (showStatusWidget && loading) {
       return Row(
         children: [
@@ -184,7 +187,7 @@ class _SendMailState extends State<SendMail> {
         ],
       );
     } else {
-    return Container();
+      return Container();
     }
   }
 }
