1fa62f68b5bf825d3f3a2e8a6c7aec88b5cfcf76
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-08-05 14:04:53 +0200

    infinite reload bug fixed

--- lib/getFirestoreData.dart
+++ lib/getFirestoreData.dart
@@ -22,110 +22,99 @@ class _FirestoreDataState extends State<FirestoreData> {
   String lastChanged = "";
 
   bool isOnline = false;
+  final DateFormat formatter = DateFormat('dd-MM-yyyy, HH:mm');
 
   _FirestoreDataState({required this.document, this.color = 0x000000});
 
-  Future<String> getStatusFromFirestore(String document) async {
-    String status = "[null]";
-    final DateFormat formatter = DateFormat('dd-MM-yyyy, HH:mm');
-    Timestamp date;
+  Future<Map<String, dynamic>> getStatusFromFirestore(String document) async {
+    Map<String, dynamic> documentFields = new Map<String, dynamic>();
+    documentFields['html'] = "<h1>ERROR!</h1>";
+    documentFields['last_changed'] = "<h1>ERROR!</h1>";
 
     await FirebaseFirestore.instance
         .collection('app')
         .doc(document)
         .get()
         .then((DocumentSnapshot ds) {
-      Map<String, dynamic> documentFields = ds.data() as Map<String, dynamic>;
-
-      status = documentFields['html'];
-      date = documentFields['last_changed'];
-      setState(() {
-        lastChanged = formatter.format(date.toDate());
-      });
+      documentFields = ds.data() as Map<String, dynamic>;
     });
-
     isOnline = await CheckConnection().isConnected();
 
-    return (status);
+    return documentFields;
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        html(document, color),
-        Text(
-          lastChanged,
-          style: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w300,
-            color: Colors.grey,
-          ),
-          textAlign: TextAlign.end,
-        ),
-      ],
-    );
-  }
-
-  Widget html(String document, int color) {
-    return FutureBuilder<String>(
-      future: getStatusFromFirestore(document),
-      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-        List<Widget> children;
-        if (snapshot.hasData) {
-          children = <Widget>[
-            Container(
-              child: new Container(
-                      child: Html(
-                          data: snapshot.data,
-                          style: {"html": Style(color: Color(color))}),
+        FutureBuilder<Map<String, dynamic>>(
+          future: getStatusFromFirestore(document),
+          builder: (BuildContext context,
+              AsyncSnapshot<Map<String, dynamic>> snapshot) {
+            List<Widget> children;
+            if (snapshot.hasData) {
+              children = <Widget>[
+                Column(children: <Widget>[
+                  Html(
+                      data: snapshot.data!['html'],
+                      style: {"html": Style(color: Color(color))}),
+                  Text(
+                    formatter.format(snapshot.data!['last_changed'].toDate()),
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.grey,
                     ),
-            ),
-          ];
-        } else if (snapshot.hasError && !isOnline) {
-          children = <Widget>[
-            Icon(
-              Icons.cloud_off,
-              color: Colors.red,
-              size: 60,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'Du bist Offline!\nBitte starte die App neu, sobald du wieder Online bist..',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.red),
+                    textAlign: TextAlign.end,
+                  ),
+                ]),
+              ];
+            } else if (snapshot.hasError && !isOnline) {
+              children = <Widget>[
+                Icon(
+                  Icons.cloud_off,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(
+                    'Du bist Offline!\nBitte starte die App neu, sobald du wieder Online bist..',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                )
+              ];
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(
+                    'Error: ${snapshot.error}',
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ];
+            } else {
+              children = <Widget>[
+                CircularProgressIndicator(),
+              ];
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: children,
               ),
-            )
-          ];
-        } else if (snapshot.hasError) {
-          children = <Widget>[
-            Icon(
-              Icons.error_outline,
-              color: Colors.red,
-              size: 60,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'Error: ${snapshot.error}',
-                textAlign: TextAlign.center,
-              ),
-            )
-          ];
-        } else {
-          children = <Widget>[
-            CircularProgressIndicator(),
-          ];
-        }
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: children,
-          ),
-        );
-      },
+            );
+          },
+        )
+      ],
     );
   }
 }
