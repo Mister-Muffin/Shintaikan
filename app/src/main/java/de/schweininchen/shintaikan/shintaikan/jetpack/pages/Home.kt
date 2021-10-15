0f3268c9fd661d24bb70b7a9d8853a39d211e6b6
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-10-15 22:19:51 +0200

    put wordpress content in card

--- app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/pages/Home.kt
+++ app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/pages/Home.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -14,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,9 +64,20 @@ fun Home(postsList: List<Array<String>>, viewModel: MyViewModel) {
             item { CircularProgressIndicator() }
         } else {
             items(postsList) { post ->
-                Text(style = Typography.h3, text = post[0])
-                Html(text = post[1])
-
+                Card(elevation = 2.dp) {
+                    Column() {
+                        Text(
+                            style = Typography.h3,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            text = post[0]
+                        )
+                        Box(Modifier.padding(top = 8.dp)) {
+                            Html(text = post[1])
+                        }
+                    }
+                }
             }
 
         }
