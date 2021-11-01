3279a59d47173d0a55c8299d5fca38c4ac829c00
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-11-01 13:22:32 +0100

    app bar color below android S

--- app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/components/mainActivity/ShintaikanAppBar.kt
+++ app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/components/mainActivity/ShintaikanAppBar.kt
@@ -1,10 +1,12 @@
 package de.schweininchen.shintaikan.shintaikan.jetpack.components.mainActivity
 
+import android.os.Build
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -16,12 +18,22 @@ fun ShintaikanAppBar(
     scope: CoroutineScope,
     scaffoldState: ScaffoldState
 ) {
+    val color =
+        TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary.copy(
+                alpha = .8f
+            ),
+            titleContentColor = Color.White,
+            navigationIconContentColor = Color.White,
+            actionIconContentColor = Color.White
+        )
     CenterAlignedTopAppBar(
         title = {
             Text(
                 appBarTitle.value, fontSize = 20.sp
             )
         },
+
         navigationIcon = {
             IconButton(onClick = { scope.launch { scaffoldState.drawerState.open() } }
             ) {
@@ -32,6 +44,9 @@ fun ShintaikanAppBar(
             /*IconButton(onClick = { *//* doSomething() *//* }) {
                          Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
                      }*/
-        }
+        },
+        colors = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) color
+        else TopAppBarDefaults.centerAlignedTopAppBarColors()
     )
+
 }
