7ec05ec269a6253e6faae2b84087cf99c9eda588
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2022-06-11 16:42:39 +0200

    Removed material 2 dependency

--- app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/NavigationDrawer.kt
+++ app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/NavigationDrawer.kt
@@ -11,14 +11,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
