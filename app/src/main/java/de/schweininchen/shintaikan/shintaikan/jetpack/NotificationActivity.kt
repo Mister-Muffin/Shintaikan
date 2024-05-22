191e56c5fde6646b840b9c5e48b16b604790c218
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2024-05-22 14:33:53 +0200

    Prototype notification request

--- app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/NotificationActivity.kt
+++ app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/NotificationActivity.kt
@@ -3,7 +3,9 @@ package de.schweininchen.shintaikan.shintaikan.jetpack
 import android.app.Activity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +20,10 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.NotificationImportant
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.WarningAmber
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -38,6 +44,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
@@ -49,6 +57,17 @@ import kotlinx.coroutines.launch
 
 @ExperimentalMaterial3Api
 class NotificationActivity : AppCompatActivity() {
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -81,19 +100,24 @@ class NotificationActivity : AppCompatActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
                         MySwitchPreference(
-                            title = "Testtile",
-                            description = "Test description",
+                            title = "Wichtige Benachichtigungen",
+                            description = "Benachichtigungen, die von uns manuell gesendet werden",
+                            Icons.Outlined.NotificationImportant,
                             checked = importantNotificationsChcked
                         ) {
-                            importantNotificationsChcked = it
+                            askNotificationPermission(requestPermissionLauncher, context)
+                            importantNotificationsChcked = !importantNotificationsChcked
                         }
                         HorizontalDivider()
                         MySwitchPreference(
                             title = "Auto Benachrichtigungen",
+                            description = "Benachrichtigungen, die automatisch durch neue Posts auf der Homepage gesendet werden",
+                            Icons.Outlined.Notifications,
                             checked = autoNotificationsChcked
                         ) {
-                            autoNotificationsChcked = it
+                            autoNotificationsChcked = !autoNotificationsChcked
                         }
+                        NotificationCard()
                     }
                 }
 
@@ -103,31 +127,86 @@ class NotificationActivity : AppCompatActivity() {
     }
 
     @Composable
+    private fun NotificationCard() {
+        Card(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.WarningAmber,
+                        contentDescription = "Warning icon",
+                        tint = Color(0xFFFFC107)
+                    )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxWidth(.75f)
+                    ) {
+                        Text(
+                            text = getString(R.string.push_problem),
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        Spacer(modifier = Modifier.padding(4.dp))
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Beheben")
+                        }
+                    }
+                    Icon(
+                        imageVector = Icons.Outlined.WarningAmber,
+                        contentDescription = "Warning icon",
+                        tint = Color(0x00000000)
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
     fun MySwitchPreference(
         title: String,
         description: String? = null,
+        icon: ImageVector,
         checked: Boolean,
-        onCheckedChange: (Boolean) -> Unit
+        onCheckedChange: () -> Unit
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.clickable {
+                onCheckedChange()
+            }
         ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Icon(
-                    Icons.Outlined.NotificationImportant,
-                    contentDescription = "",
+                    icon,
+                    contentDescription = null,
                     modifier = Modifier.size(24.dp)
                 )
                 Spacer(modifier = Modifier.width(8.dp))
-                Column(Modifier.padding(16.dp)) {
+                Column(
+                    Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(.8f)
+                ) {
                     Text(
                         text = title,
                         textAlign = TextAlign.Start,
                     )
                     if (description != null) {
                         Text(
-                            text = "Erhalte Benachrichtigungen",
+                            text = description,
                             style = MaterialTheme.typography.bodySmall,
                             modifier = Modifier.padding(
                                 vertical = 4.dp
@@ -136,12 +215,11 @@ class NotificationActivity : AppCompatActivity() {
                         )
                     }
                 }
-
             }
             Spacer(modifier = Modifier.weight(1f))
             Switch(
                 checked = checked,
-                onCheckedChange = onCheckedChange
+                onCheckedChange = { onCheckedChange() }
             )
         }
     }
---
+++ app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/Notifications.kt
@@ -0,0 +1,37 @@
+package de.schweininchen.shintaikan.shintaikan.jetpack
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
+import androidx.core.content.ContextCompat
+
+fun askNotificationPermission(
+    requestPermissionLauncher: ActivityResultLauncher<String>,
+    context: Context
+) {
+    val activity = context as Activity
+    // This is only necessary for API level >= 33 (TIRAMISU)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            // FCM SDK (and your app) can post notifications.
+        } else if (shouldShowRequestPermissionRationale(
+                activity,
+                Manifest.permission.POST_NOTIFICATIONS
+            )
+        ) {
+            // TODO: display an educational UI explaining to the user the features that will be enabled
+            //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+            //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+            //       If the user selects "No thanks," allow the user to continue without notifications.
+        } else {
+            // Directly ask for the permission
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+}
