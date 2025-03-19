c02aa6a9300d5ce0a7959be0a347810f10327e61
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2025-03-19 17:22:31 +0100

    Add a divider and a title "Aktuelles" to the home screen

--- app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/pages/Home.kt
+++ app/src/main/java/de/schweininchen/shintaikan/shintaikan/jetpack/pages/Home.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -199,6 +200,16 @@ fun Home(
                 )
             }
         } else {
+            item {
+                HorizontalDivider()
+            }
+            item {
+                Text(
+                    text = "Aktuelles",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+            }
             items(postsList) { post ->
                 Card(modifier = Modifier.fillMaxWidth()) {
                     Column(
